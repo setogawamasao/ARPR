15e88e0c0e37cc15bb208c7d3435711ea487eb6a
setogawamasao <ms.job.hunting@gmail.com>
2022-03-16 01:21:13 +0900

    add touch new object

--- ARPR/ViewController.swift
+++ ARPR/ViewController.swift
@@ -22,10 +22,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         var position : SCNVector3
         var isAnswered : Bool = false
     }
-    var qas:[Qa] = [Qa(question: "年齢は?", answer: "32歳", position: SCNVector3(-0.05,0.25,-0.3)),
-                    Qa(question: "学歴は?", answer: "院卒", position: SCNVector3(0.05,0.25,-0.3)),
-                    Qa(question: "職業は?", answer: "SE", position: SCNVector3(-0.05,-0.05,-0.3)),
-                    Qa(question: "年収は?", answer: "600", position: SCNVector3(0.05,-0.05,-0.3))]
+    var qas:[Qa] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,26 +36,17 @@ class ViewController: UIViewController, ARSessionDelegate {
         sceneView.session.run(config, options: [.removeExistingAnchors])
         
         // ドラッグ＆ドロップの画面上のジェスチャーを検知
-sceneView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(ViewController.handleMove(_:))))
+        sceneView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(ViewController.handleMove(_:))))
+        sceneView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTapped(_:))))
         
         // ライトの追加
         sceneView.autoenablesDefaultLighting = true
         
-        for qa in qas {
-            let text = SCNText(string: qa.question, extrusionDepth: 0.5)
-            text.font = UIFont(name: "HiraginoSans-W6", size: 1 )
-            text.firstMaterial?.diffuse.contents = UIColor.green
-            let textNode = SCNNode(geometry: text)
-            
-            let (min, max) = (textNode.boundingBox)
-            let w = Float(max.x - min.x)
-            let h = Float(max.y - min.y)
-            textNode.pivot = SCNMatrix4MakeTranslation(w/2 + min.x, h/2 + min.y, 0)
-            textNode.position = qa.position
-            textNode.scale = SCNVector3(0.02,0.02,0.02)
-            
-            sceneView.scene.rootNode.addChildNode(textNode)
-        }
+        // 初期QAの追加
+        self.AddText(qa:Qa(question: "年齢は?", answer: "32歳", position: SCNVector3(-0.05,0.25,-0.3)))
+        self.AddText(qa:Qa(question: "学歴は?", answer: "院卒", position: SCNVector3(0.05,0.25,-0.3)))
+        self.AddText(qa:Qa(question: "職業は?", answer: "SE", position: SCNVector3(-0.05,-0.05,-0.3)))
+        self.AddText(qa:Qa(question: "年収は?", answer: "600", position: SCNVector3(0.05,-0.05,-0.3)))
     }
     
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
@@ -85,10 +73,35 @@ sceneView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #sel
     // ドラッグ&ドロップのジェスチャーの挙動
     @objc func handleMove(_ gesture: UIPanGestureRecognizer) {
         let location = gesture.location(in: self.sceneView)
-        guard let nodeHitTest = self.sceneView.hitTest(location, options: nil).first else { print("no node"); return }
+        guard let nodeHitTest = self.sceneView.hitTest(location, options: nil).first else {
+            print("no node");
+            return
+        }
         let nodeHit = nodeHitTest.node
         let worldTransform = nodeHitTest.simdWorldCoordinates
         nodeHit.position = SCNVector3(worldTransform.x, worldTransform.y, -0.3)
+    }
+    
+    // タップのジェスチャーの挙動
+    @objc func handleTapped(_ gesture: UIHoverGestureRecognizer) {
+        let location = gesture.location(in: self.sceneView)
+        guard let nodeHitTest = self.sceneView.hitTest(location, options: nil).first else {
+            // no hit object -> add new qa object
+            let addPlane = SCNVector3(x: 0, y: 0, z: -0.3)
+            guard let cameraNode = sceneView.pointOfView else { return }
+            let pointInWorld = cameraNode.convertPosition(addPlane, to: nil)
+            var screenPosition = sceneView.projectPoint(pointInWorld)
+            screenPosition.x = Float(location.x)
+            screenPosition.y = Float(location.y)
+            let newPosition = sceneView.unprojectPoint(screenPosition)
+            self.AddText(qa:Qa(question: "追加した?", answer: "追加しました", position: newPosition))
+            return
+        }
+        
+        
+        // hit object -> edit qa object
+        print("exist node");
+        
     }
     
     func getHandPosition(handPoseObservation: VNHumanHandPoseObservation) {
@@ -117,6 +130,23 @@ sceneView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #sel
                 q[0].isAnswered = true
             }
         }
+    }
+    
+    func AddText(qa:Qa){
+        qas.append(qa)
+        let text = SCNText(string: qa.question, extrusionDepth: 0.5)
+        text.font = UIFont(name: "HiraginoSans-W6", size: 1 )
+        text.firstMaterial?.diffuse.contents = UIColor.green
+        let textNode = SCNNode(geometry: text)
+        
+        let (min, max) = (textNode.boundingBox)
+        let w = Float(max.x - min.x)
+        let h = Float(max.y - min.y)
+        textNode.pivot = SCNMatrix4MakeTranslation(w/2 + min.x, h/2 + min.y, 0)
+        textNode.position = qa.position
+        textNode.scale = SCNVector3(0.02,0.02,0.02)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
     }
     
 }
