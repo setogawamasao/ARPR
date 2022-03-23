2549a620d55f89b471c7bb101edc41ce15ec0256
setogawamasao <ms.job.hunting@gmail.com>
2022-03-23 23:47:10 +0900

    add splash

--- ARPR/AppDelegate.swift
+++ ARPR/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // スプラッシュを1秒表示する
+        sleep(1)
         return true
     }
 
